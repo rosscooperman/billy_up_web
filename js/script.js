eca4a2eb157e2f33771f4587c9b0f0518bfeddc6
Brett Mascavage <brett@eastmedia.com>
2012-07-30 18:06:29 -0400

    updated slideshow

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-/* Author:
+$('#slideshow').cycle({
+    fx: 'scrollHorz',
+    speed: 200,
+    timeout: 5000,
+    pause: 1,
+    cleartype: true,
+    cleartypeNoBg: true,
+    startingSlide: 5,
+    pager: '#slideshow-nav',
+    updateActivePagerLink: function(pager, currSlideIndex) { 
+        $(pager).find('li').removeClass('active') 
+            .filter('li:eq('+currSlideIndex+')').addClass('active'); },
+    pagerAnchorBuilder: function(idx, slide) {
+        return '#slideshow-nav li:eq(' + (idx) + ')';
+    }
+});
 
-*/
-
+$('#slideshow-nav li').click(function() { 
+    $('#slideshow').cycle('pause');
+    setTimeout(function(){
+      $('#slideshow').cycle('resume');
+    }, 5000);
+});
